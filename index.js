7dbf434d9ff8afc42cadde54126c23b8d5425d90
Chris Grayson <chris@activeprospect.com>
2016-05-11 15:46:55 -0500

    add GET query integration; fixes #16

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   outbound: {
     form: require('./lib/form'),
     json: require('./lib/json'),
+    query: require('./lib/query'),
     xml: require('./lib/xml'),
     soap: require('./lib/soap')
   }
